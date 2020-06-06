77d6a80a6c98ef5951dd854c4dd3e08b32441cd5
Liz Lam <lizlam.217@gmail.com>
2020-06-06 02:00:11 -0700

    Initial add of Angel Chess site.

---
+++ src/routes/about.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { fade } from 'svelte/transition';
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		font-size: 1.5em;
+		max-width: 340px;
+		margin: 0 auto;
+	}
+
+	li {
+		text-align: left;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+<!-- Chapter 14 of Encyclopedia of Chess Variants-->
+<main in:fade>
+	<h1>About</h1>
+	<p>A common way to enhance the traditonal game of chess is by adding more powerful pieces to the board we are all so familiar with. 
+
+	Angel Chess is played on a 9x8 board and adds a powerful Angel piece with the combination move of a Queen and a Knight.  There is also an additional
+	pawn that stands "guard" in front of it as well.<br /><br /> Most rules of the basic game applies with the following differences:
+    </p>
+    <ul>
+       <li>The Angel piece sits to the right of the King.</li>
+	   <li>Pawn promotion to Angel on e-file only. (the King's lane)</li>
+	   <li>When Queen castling, the King goes to the Knight's square.</li>
+	   <li>When King castling, the King goes to the Bishop's square.</li>
+       <li>You can not capture the opposing side's Angel if your Angel piece will be captured on the following move. (No Angel for Angel sacrifice)</li>
+	   <li>Supplementary: The Angel piece can not check from a square where it can be taken by any piece except the King.</li>
+	</ul>
+
+	
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { fade } from 'svelte/transition';
+</script>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		font-size: 1.5em;
+		max-width: 540px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Angel Chess</title>
+</svelte:head>
+
+<main in:fade>
+	<h1>Angel Chess</h1>
+	<p>Angel Chess is a chess variation created in the early 1900's by <br /> Louis P. d'Autremont.</p>
+        <img alt="Angel Chess Patent" src="angel_chess_patent.png" />
+        <p>It is played on a 9x8 board with an extra pawn and Angel piece.<br />  
+          The Angel moves like a Queen and Knight with some special rules around capturing. <br />
+          This site is dedicated to the exploration of this game. </p>
+</main>
