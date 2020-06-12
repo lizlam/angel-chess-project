aa8478c265308ea4ad3db5eae80619accce7be55
Liz Lam <lizlam.217@gmail.com>
2020-06-11 19:00:06 -0700

    Fix segment.

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -52,7 +52,7 @@
 	<ul>
 		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
 		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-		<li><a href="play">play</a></li>
+		<li><a aria-current="{segment === 'play' ? 'page' : undefined}" href="play">play</a></li>
 
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
