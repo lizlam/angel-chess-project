92706418142bf982437e82046588dca587aa408d
Liz Lam <lizlam.217@gmail.com>
2020-06-21 12:25:33 -0700

    Add World of Chess reference.

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -49,9 +49,10 @@
 	<p>Most of what know about Angel Chess is from Louis P. d'Autremont's <a href="angel_chess_patent_v1.pdf">patent</a>.  Among other things stated in the patent, his aim was to improve the game of chess by reaching
 the "thick of the game" more quickly.  This would seem to coincide with the mobilization phase that basically starts the game 20 moves in.  He also wanted a game where the stronger player was able to defeat the weaker player more quickly.  Having an extremely powerful piece like the "destroying angel" would certainly contribute to this goal.  The patent has good insight on what motivated Louis P. d'Autremont to create this game and I would recommend to anyone interested to give it a read. </p>
 
-        <p>Angel Chess also makes a small appearance in Chapter 14 of The Classified Encyclopedia of Chess 
+        <p>Angel Chess also makes a small appearance in <a href="ch14_encyc.pdf">Chapter 14</a> of The Classified Encyclopedia of Chess 
         Variants by David Pritchard. The book is out of print but available in PDF form on
         <a href="http://www.jsbeasley.co.uk/">John and Sue Beasley's Website</a>. There seems to be a small descrepancy where Pritchard dates the game to 1918 in oppose to the patent date of 1915.  It's hard to say why without the leaflet or the British Chess Magazine referred to.</p>
         <img alt="Encyclopedia excerpt" width="300" src="angel_chess_encyc_entry.png" />
-      </div>
+      <p>An even smaller appearance is made in the Notes sections of <a href="http://aworldofchess.com/">A World of Chess</a> by Rick Knowlton and Jean-Louis Cazaux.</p>
+      <img alt="World of Chess Notes" width="300" src="woc-notes-angelchess.png" />
 </main>      
