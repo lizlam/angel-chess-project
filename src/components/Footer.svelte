e823a84681b8f3998dfa634eae50a1723e9f5cfd
Liz Lam <lizlam.217@gmail.com>
2020-07-24 00:42:08 -0700

    Add footer.

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+  let date = new Date();
+  let year = date.getFullYear();
+</script>
+
+<style>
+  p {
+    text-align: left;
+    margin: 0 30px;
+  }
+</style>
+
+<footer>
+  <p>© {year} A <a href="https://richgames.xyz/">Rich Games</a> Site</p>
+</footer>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import Footer from '../components/Footer.svelte';
 
 	export let segment;
 </script>
@@ -19,4 +20,5 @@
 
 <main>
 	<slot></slot>
+        <Footer />
 </main>
